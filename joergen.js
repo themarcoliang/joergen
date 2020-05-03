207ee99a49cc6d10b153614d133921201f9b4270
themarcoliang <marcoliang333@gmail.com>
2020-05-02 23:56:58 -0700

    Basic Message Receiving Cases

--- joergen.js
+++ joergen.js
@@ -11,8 +11,21 @@ client.on('ready', ()=> {
 });
 
 client.on('message', msg => {
-    if(msg.content === 'ping'){
-        msg.reply('Pong!');
+    const message = msg.content.toLowerCase();
+    const split_message = message.split(' ');
+    if(message.startsWith('joergen') && split_message.length > 1) //only care if message starts with joergen and has more than one word
+    {
+        const arg = split_message.slice(1,2).join(' ');
+        // console.log(arg);
+        if(arg === 'play')
+        {
+            console.log('playing');
+        }
+        else if(arg == 'stop')
+        {
+            console.log('stopping');
+        }
+
     }
 });
 
