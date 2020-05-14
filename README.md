d2730fa198c4634a97ff5daf7f8d660f61d95a87
themarcoliang <marcoliang333@gmail.com>
2020-05-14 15:14:03 -0700

    Changed keyword to '!'

--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 A private discord bot that automatically searches and plays youtube music based on user commands
 
 Supported commands:
-[KEYWORD] play [query] : streams the requested video, or queues it if a song is already playing, query could be either a search term or link to youtube
+!play [query] : streams the requested video, or queues it if a song is already playing, query could be either a search term or link to youtube
 
-[KEYWORD] play : resumes player if paused, does nothing if player's not paused
+!play : resumes player if paused, does nothing if player's not paused
 
-[KEYWORD] pause : pauses the player
+!pause : pauses the player
 
-[KEYWORD] stop : clears the queue, stops the player, and exits the voice channel
+!stop : clears the queue, stops the player, and exits the voice channel
 
-[KEYWORD] skip : skips current song, if nothing else is queued, bot leaves
-
-KEYWORD is set to 'joergen' by default
+!skip : skips current song, if nothing else is queued, bot leaves
--- joergen.js
+++ joergen.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+ const fs = require('fs');
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const {google} = require('googleapis');
@@ -6,11 +6,10 @@ const {google} = require('googleapis');
 
 var dispatcher = null;
 var channel = null;
-var playing = false;
 var latestMessage = null;
 var queue = [];
 
-const KEYWORD = 'joergen';
+// const KEYWORD = 'joergen';
 const keys = JSON.parse(fs.readFileSync('./keys.json'));
 const TOKEN = keys.discord_token;
 const YT_KEY = keys.yt_key;
@@ -54,7 +53,6 @@ function playVideo(response){
         let stream = ytdl('https://www.youtube.com/watch?v=' + id, islive ? { quality: [128,127,120,96,95,94,93] } : {highWaterMark: 1<<25, filter: 'audioonly' });
         try{
             dispatcher = connection.play(stream, {highWaterMark: 1});
-            playing = true;
         }catch(err){
             console.error(err);
             return;
@@ -69,7 +67,6 @@ function playVideo(response){
             if(queue.length == 0) //queue is empty
             {
                 console.log('Finished playing');
-                playing = false;
                 channel.leave();
             }
             else //more songs to play
@@ -87,15 +84,20 @@ function playVideo(response){
 
 //Function to pause player
 function pauseVideo(){
-    dispatcher.pause();
-    latestMessage.channel.send("K I'm pausing");
-    console.log('Pausing playback');
-    playing = false;
+    if(dispatcher!=null)
+    {
+        dispatcher.pause();
+        latestMessage.channel.send("K I'm pausing");
+        console.log('Pausing playback');
+    }
+    else{
+        latestMessage.reply("nothing's playing dude");
+    }
 }
+        
 
 //Function to stop currently playing video, and exit voice channel if queue is empty
 function stopVideo(){
-    playing = false;
     if(dispatcher!=null)
     {
         dispatcher.end();
@@ -109,6 +111,16 @@ function stopVideo(){
     }
 }
 
+function verifyChannel(){
+    if(!latestMessage.member.voice.channel)  //check if user is in a voice channel (required)
+    {
+        latestMessage.reply('you must be in a voice channel to do that!');
+        console.log('Failed to play: user not in a voice channel');
+        return false;
+    }
+    return true;
+}
+
 //Joergen is ready
 client.on('ready', ()=> {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -119,21 +131,17 @@ client.on('message', async function (msg) {
     latestMessage = msg;
     const message = latestMessage.content.toLowerCase();
     const split_message = message.split(' ');
-
-    if(message.startsWith(KEYWORD) && split_message.length > 1) //only care if message starts with joergen and has more than one word
+    console.log("message received");
+    if(!latestMessage.author.bot) //only care if not from bot
     {
-        if(!latestMessage.member.voice.channel)  //check if user is in a voice channel (required)
+        const arg = split_message.slice(0,1).join(' '); //get arguments for joergen
+        console.log(arg);
+        if(arg === '!play')
         {
-            latestMessage.reply('you must be in a voice channel to do that!');
-            console.log('Failed to play: user not in a voice channel');
-            return;
-        }
-        const arg = split_message.slice(1,2).join(' '); //get arguments for joergen
-        if(arg === 'play')
-        {
-            const query = split_message.slice(2).join(' ');
+            if(!verifyChannel()) return;
+            const query = split_message.slice(1).join(' ');
             
-            if(query=='' ) //if no query is provided
+            if(query=='') //if no query is provided
             {
                 if(dispatcher==null || !dispatcher.paused) //player's not paused
                 {
@@ -145,7 +153,6 @@ client.on('message', async function (msg) {
                     dispatcher.resume();
                     console.log('Resuming');
                     latestMessage.channel.send('K, resuming');
-                    playing = true;
                     return;
                 }
                 
@@ -163,23 +170,26 @@ client.on('message', async function (msg) {
                 latestMessage.channel.send("Something's playing already, I'll queue " + response.data.items[0].snippet.title + " for later");
             }
         }
-        else if(arg == 'pause')
+        else if(arg == '!pause')
         {
+            if(!verifyChannel()) return;
             console.log('Pausing');
             pauseVideo();
         }
-        else if(arg == 'stop')
+        else if(arg == '!stop')
         {
+            if(!verifyChannel()) return;
             queue = []; //clear queue
             console.log('Stopping');
             latestMessage.channel.send('K, stopping...');
             stopVideo();
         }
-        else if(arg == 'skip')
+        else if(arg == '!skip')
         {
+            if(!verifyChannel()) return;
             if(dispatcher==null || dispatcher.paused)
             {
-                latestMessage.reply("Nothing is playing though");
+                latestMessage.reply("nothing is playing though");
             }
             else
             {
